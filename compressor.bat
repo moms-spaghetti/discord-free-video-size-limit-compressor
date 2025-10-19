b7218ad6fae6941d20a609c1bf1415b26ce2438e
moms-spaghetti <williamedwards36@aol.com>
2025-10-19 22:44:59 +0100

    adjust way params handled, lower nvenc overshoot for smaller files

--- compressor.bat
+++ compressor.bat
@@ -1,7 +1,11 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+:: set these 
+set "DEFAULT_AUDIO_BOOST=22"
+
 :: config
+set "PROBE_RES="
 set "TARGET_MB=9"
 set "AUDIO_KBPS=128"
 set "AUDIO_VOLUME=-af volume=0dB"
@@ -23,43 +27,54 @@ set "MSG_SCALE_FILTER_VALUES=MSG_SCALE_FILTER_VALUES: video_scale supported valu
 set "MSG_SCALE_FILTER_IGNORE=MSG_SCALE_FILTER_IGNORE: video_scale if only applying audio_boost pass an underscore _"
 set "MSG_AUDIO_VOLUME_VALUES=MSG_AUDIO_VOLUME_VALUES: audio_boost supported values between 1-30"
 
+:: video file width
+set "CMD=ffprobe -v error -select_streams v:0 -show_entries stream=width -of csv=p=0 "%INPUT%""
+for /f "usebackq delims=" %%a in (`!CMD!`) do set "PROBE_RES=%%a"
+
+:: file
 if "%~1"=="" (
     echo %ERR_USAGE% 
     echo %ERR_NO_FILE_NAME%
     exit /b 1
 )
 
-:: scale filter
-if not "%~2"=="" (
-    if not "%~2"=="_" (
-        set "AUDIO_KBPS=96"
-        if /i "%~2"=="1440" (
-            set "SCALE_FILTER=-vf scale=-2:1440"
-        ) else if /i "%~2"=="1080" (
-            set "SCALE_FILTER=-vf scale=-2:1080"
-        ) else if /i "%~2"=="900" (
-            set "SCALE_FILTER=-vf scale=-2:900"
-        ) else if /i "%~2"=="720" (
-            set "SCALE_FILTER=-vf scale=-2:720"
-        ) else if /i "%~2"=="640" (
-            set "SCALE_FILTER=-vf scale=-2:640"
-        ) else if /i "%~2"=="540" (
-            set "SCALE_FILTER=-vf scale=-2:540"
-        ) else if /i "%~2"=="480" (
-            set "SCALE_FILTER=-vf scale=-2:480"
-        ) else if /i "%~2"=="360" (
-            set "SCALE_FILTER=-vf scale=-2:360"
-        ) else (
-            echo %ERR_SCALE_FILTER_UNKNOWN%
-            echo %MSG_SCALE_FILTER_VALUES%
-            echo %MSG_SCALE_FILTER_IGNORE%
-            exit /b 1
-        )   
-    )
+:: video size
+if "%~2"=="" (
+	set "SCALE_FILTER=-vf scale=-2:%PROBE_RES%"
+) else if "%~2"=="_" (
+		set "SCALE_FILTER=-vf scale=-2:%PROBE_RES%"
+	) else (
+		set "AUDIO_KBPS=96"
+			if /i "%~2"=="1440" (
+				set "SCALE_FILTER=-vf scale=-2:1440"
+			) else if /i "%~2"=="1080" (
+				set "SCALE_FILTER=-vf scale=-2:1080"
+			) else if /i "%~2"=="900" (
+				set "SCALE_FILTER=-vf scale=-2:900"
+			) else if /i "%~2"=="720" (
+				set "SCALE_FILTER=-vf scale=-2:720"
+			) else if /i "%~2"=="640" (
+				set "SCALE_FILTER=-vf scale=-2:640"
+			) else if /i "%~2"=="540" (
+				set "SCALE_FILTER=-vf scale=-2:540"
+			) else if /i "%~2"=="480" (
+				set "SCALE_FILTER=-vf scale=-2:480"
+			) else if /i "%~2"=="360" (
+				set "SCALE_FILTER=-vf scale=-2:360"
+			) else (
+				echo %ERR_SCALE_FILTER_UNKNOWN%
+				echo %MSG_SCALE_FILTER_VALUES%
+				echo %MSG_SCALE_FILTER_IGNORE%
+				exit /b 1 
+	)
 )
 
 :: audio volume
-if not "%~3"=="" (
+if "%~3"=="" (
+	set "AUDIO_VOLUME=-af volume=%DEFAULT_AUDIO_BOOST%dB"
+) else if "%~3"=="_" (
+	set "AUDIO_VOLUME=-af volume=%DEFAULT_AUDIO_BOOST%dB"
+) else (
     set "VOLUME_ARG=%~3"
 
     :: test if number
@@ -96,8 +111,8 @@ for /f %%a in ('ffprobe -v error -select_streams v:0 -show_entries format^=durat
 for /f "tokens=1 delims=." %%b in ("%DURATION%") do set "DURATION=%%b"
 
 :: eval bitrate
-set /a TOTAL_KBPS=%TARGET_MB:.=0%*8192/%DURATION%
-set /a SAFE_TOTAL_KBPS=%TOTAL_KBPS% * 92 / 100
+set /a TOTAL_KBPS=%TARGET_MB%*8389/%DURATION%
+set /a SAFE_TOTAL_KBPS=%TOTAL_KBPS% * 85 / 100
 set /a VIDEO_KBPS=%SAFE_TOTAL_KBPS% - %AUDIO_KBPS%
 set /a BUF_KBPS=%VIDEO_KBPS%
 
@@ -110,5 +125,6 @@ ffmpeg -y -i "%INPUT%" -c:v h264_nvenc -rc cbr -preset slow ^
 :: 1st pass logs remove
 del ffmpeg2pass-0.log 2>nul
 del ffmpeg2pass-0.log.mbtree 2>nul
+
 
 endlocal
